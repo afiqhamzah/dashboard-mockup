cee4db66a0adb567223e2963a547fb3fec40cd2b
afiqhamzah <afiq.1991@gmail.com>
2017-04-16 22:41:43 +0800

    dashboard initial layout v0.1

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard layout</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <style media="screen">
+      .outer-frame-a{
+        border-color: red;
+        background-color:#007f00;
+        color: white;
+        padding: 1.5em;
+        text-decoration: underline;
+      }
+
+      .outer-frame-b{
+        border-color: blue;
+        background-color: #001900;
+        color: white;
+        padding:1.5em;
+      }
+
+      div {
+        border: 1px solid white;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8 outer-frame-a">
+          WEEKLY DASHBOARD
+        </div>
+        <div class="col-md-4 outer-frame-b">
+          Received Letter of award for Batang Lupar Project
+        </div>
+        <div class="col-md-8 outer-frame-c">
+          <div class="row"> <!-- START local stock -->
+            <div class="col-md-2" style="background-color:orange; height:2.5em;">
+
+            </div>
+            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3" style="background-color:orange; height:13em;">
+
+            </div>
+            <div class="col-md-3" style="background-color:grey; height: 13em;">
+
+            </div>
+            <div class="col-md-6" style="background-color:purple; height:13em;">
+
+            </div>
+          </div> <!-- END local stock -->
+          <div class="row"> <!-- START sales -->
+            <div class="col-md-2" style="background-color:orange; height:2.5em;">
+
+            </div>
+            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6" style="background-color:orange; height:13em;">
+
+            </div>
+            <div class="col-md-6" style="background-color:grey; height: 13em;">
+
+            </div>
+          </div> <!-- END sales -->
+          <div class="row"> <!-- START paddy procurement -->
+            <div class="col-md-2" style="background-color:orange; height:2.5em;">
+
+            </div>
+            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3" style="background-color:orange; height:13em;">
+
+            </div>
+            <div class="col-md-3" style="background-color:grey; height: 13em;">
+
+            </div>
+            <div class="col-md-6" style="background-color:purple; height:13em;">
+
+            </div>
+          </div> <!-- END paddy procurement -->
+          <div class="row"> <!-- START rice stock position -->
+            <div class="col-md-2" style="background-color:orange; height:2.5em;">
+
+            </div>
+            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6" style="background-color:orange; height:13em;">
+
+            </div>
+            <div class="col-md-6" style="background-color:grey; height: 13em;">
+
+            </div>
+          </div>
+        </div> <!-- END paddy rice stock position -->
+
+        <div class="col-md-4 outer-frame-d">
+          <div class="row"> <!-- START operation key highlight -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>OPERATIONAL KEY HIGHLIGHT</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              operational key highlight
+            </div>
+          </div> <!-- END operation key highlight -->
+          <div class="row"> <!-- START reportable events -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>Reportable events</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              Reportable events
+            </div>
+          </div> <!-- END reportable events -->
+          <div class="row"> <!-- START staff matters -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>staff matters</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              staff matters
+            </div>
+          </div> <!-- END staff matters -->
+          <div class="row"> <!-- START government meetings -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>government meetings</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              government meetings
+            </div>
+          </div> <!-- END government meetings -->
+          <div class="row"> <!-- START governments owing -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>government owing</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              government owing
+            </div>
+          </div> <!-- END government owing -->
+          <div class="row"> <!-- START forex -->
+            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
+              <p>government forex</p>
+            </div>
+            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
+              government forex
+            </div>
+          </div> <!-- END forex -->
+        </div>
+      </div>
+    </div>
+  <!--load javascript  -->
+  <script src="vendor/twbs/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+  </body>
+</html>
