fc9406e4474cc9a76ae7f7f803f0e426a0a0922d
afiqhamzah <afiq.1991@gmail.com>
2017-04-20 17:41:28 +0800

    using BEM CSS organization

--- index.php
+++ index.php
@@ -1,165 +1,227 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Dashboard layout</title>
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <style media="screen">
-      .outer-frame-a{
-        border-color: red;
-        background-color:#007f00;
-        color: white;
-        padding: 1.5em;
-        text-decoration: underline;
-      }
-
-      .outer-frame-b{
-        border-color: blue;
-        background-color: #001900;
-        color: white;
-        padding:1.5em;
-      }
-
-      div {
-        border: 1px solid white;
-      }
-    </style>
+    <meta charset="utf-9">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="asset/styles.css">
   </head>
   <body>
     <div class="container-fluid">
+      <!-- START header -->
       <div class="row">
-        <div class="col-md-8 outer-frame-a">
+        <div class="col-md-9 text-left header">
           WEEKLY DASHBOARD
         </div>
-        <div class="col-md-4 outer-frame-b">
-          Received Letter of award for Batang Lupar Project
-        </div>
-        <div class="col-md-8 outer-frame-c">
-          <div class="row"> <!-- START local stock -->
-            <div class="col-md-2" style="background-color:orange; height:2.5em;">
-
-            </div>
-            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
-
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-3" style="background-color:orange; height:13em;">
-
-            </div>
-            <div class="col-md-3" style="background-color:grey; height: 13em;">
-
-            </div>
-            <div class="col-md-6" style="background-color:purple; height:13em;">
-
-            </div>
-          </div> <!-- END local stock -->
-          <div class="row"> <!-- START sales -->
-            <div class="col-md-2" style="background-color:orange; height:2.5em;">
-
-            </div>
-            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
-
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6" style="background-color:orange; height:13em;">
-
-            </div>
-            <div class="col-md-6" style="background-color:grey; height: 13em;">
-
-            </div>
-          </div> <!-- END sales -->
-          <div class="row"> <!-- START paddy procurement -->
-            <div class="col-md-2" style="background-color:orange; height:2.5em;">
-
-            </div>
-            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
-
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-3" style="background-color:orange; height:13em;">
-
-            </div>
-            <div class="col-md-3" style="background-color:grey; height: 13em;">
-
-            </div>
-            <div class="col-md-6" style="background-color:purple; height:13em;">
-
-            </div>
-          </div> <!-- END paddy procurement -->
-          <div class="row"> <!-- START rice stock position -->
-            <div class="col-md-2" style="background-color:orange; height:2.5em;">
-
-            </div>
-            <div class="col-md-10" style="background-color:grey; height: 2.5em;">
-
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6" style="background-color:orange; height:13em;">
-
-            </div>
-            <div class="col-md-6" style="background-color:grey; height: 13em;">
-
-            </div>
-          </div>
-        </div> <!-- END paddy rice stock position -->
-
-        <div class="col-md-4 outer-frame-d">
-          <div class="row"> <!-- START operation key highlight -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>OPERATIONAL KEY HIGHLIGHT</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              operational key highlight
-            </div>
-          </div> <!-- END operation key highlight -->
-          <div class="row"> <!-- START reportable events -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>Reportable events</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              Reportable events
-            </div>
-          </div> <!-- END reportable events -->
-          <div class="row"> <!-- START staff matters -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>staff matters</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              staff matters
-            </div>
-          </div> <!-- END staff matters -->
-          <div class="row"> <!-- START government meetings -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>government meetings</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              government meetings
-            </div>
-          </div> <!-- END government meetings -->
-          <div class="row"> <!-- START governments owing -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>government owing</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              government owing
-            </div>
-          </div> <!-- END government owing -->
-          <div class="row"> <!-- START forex -->
-            <div class="col-md-12" style="background-color:gold; color:white; height:1.5em;">
-              <p>government forex</p>
-            </div>
-            <div class="col-md-12" style="background-color:gold; color:beige; height:10em;">
-              government forex
-            </div>
-          </div> <!-- END forex -->
+        <div class="col-md-3 text left header header--nav">
+          Received letter for award for Batang Lupar Project
         </div>
       </div>
+      <!-- END header -->
+      <!-- START body main -->
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-12 box">
+              <div class="row">
+                <div class="col-md-2 box__title box__title--orange">
+
+                </div>
+                <div class="col-md-10 box__title box__title--placeholder"></div>
+              </div>
+              <div class="row">
+                <div class="col-md-3">
+                  graph
+                </div>
+                <div class="col-md-3">
+                  graph
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 box">
+              <div class="row">
+                <div class="col-md-2 box__title box__title--green">
+                  SALES
+                </div>
+                <div class="col-md-10 box__title box__title--placeholder">YTD Total Sales: Slightly below plan due to unpredictable weather</div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 box">
+              <div class="row">
+                <div class="col-md-2 box__title box__title--indigo">
+                  PADDY PROCUREMENT
+                </div>
+                <div class="col-md-10 box__title box__title--placeholder">Private millers has started competing actively in the market due to the implementation of actual grading pemutuan sebenar</div>
+              </div>
+              <div class="row">
+                <div class="col-md-3">
+                  graph
+                </div>
+                <div class="col-md-3">
+                  graph
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 box">
+              <div class="row">
+                <div class="col-md-2 box__title box__title--blue">
+                  RICE STOCK POSITION
+                </div>
+                <div class="col-md-10 box__title box__title--placeholder">Trading stock (import + local) turnover is averaging at xx months [xx1K/one month sales (xxK MT)], is higher than the ideal position of x month</div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      <!-- END body main -->
+      <!-- START body nav -->
+        <div class="col-md-3">
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  OPERATION KEY HIGHLIGHTS
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  REPORTABLE EVENTS
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  STAFF MATTERS
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  GOVERNMENT MEETING
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  GOVERNMENT OWING IN RM (Million)
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 minibox">
+              <div class="row">
+                <div class="col-md-12 box__title box__title--darkorange">
+                  FOREX
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  table
+                </div>
+                <div class="col-md-6">
+                  table
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      <!-- END body nav -->
+      </div>
     </div>
-  <!--load javascript  -->
-  <script src="vendor/twbs/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="node_modules/jquery/dist/jquery.js" charset="utf-9"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js" charset="utf-9"></script>
   </body>
 </html>
